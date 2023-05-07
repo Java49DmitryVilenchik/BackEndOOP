c26e52edfdcc4d5896c91c2febbf5f20f3aefc51
DmitryVilenchik <vilend1969@gmail.com>
2023-05-07 20:49:14 +0300

    Change-Id: I0000000000000000000000000000000000000000
    Change-Id: Iecb204eac486b602305c40c4a8b4c4a2479f2335

--- src/telran/algoritm/initialAlgoritm.java
+++ src/telran/algoritm/initialAlgoritm.java
@@ -1,5 +1,8 @@
 package telran.algoritm;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class initialAlgoritm {
 	public static void sortShortPositive(short [] array) {
 		//array of the positive short numbers
@@ -17,17 +20,98 @@ public class initialAlgoritm {
 			}
 		}
 	}
-		public static boolean isSumTwo(short[] array, short sum) {
-			//returns true if there are two numbers in the given array,
-			//sum of which equals the given sum value
-			//othervise false
-			return false;
+	public static boolean isSumTwo(short[] array, short sum) {
+		int helperSize=sum < 0 ? Short.MAX_VALUE+1 : sum+1;
+		boolean[] helper=new boolean[helperSize];
+		boolean res=false;
+		int index=0;
+		while (index<array.length && !res) {
+			short value=array[index];
+			short secondValue=(short)(sum-value);
+			if (secondValue>=0) {
+				if (helper[secondValue]) {
+					res=true;
+				}
+				else {
+					helper[value]=true;
+				}
 		}
-		public static short getMaxPositiveWithNegativeReflect(short[] array) {
-			//return maximal positive number for whict there is negative image
-			//if there not such numbers at all the method return
-			return -1;
+			index++;
+			
+	}
+		return res;
+	}	
+	
+		//public static boolean isSumTwo(short[] array, short sum) {
+		//	boolean[] seen = new boolean[Short.MAX_VALUE+1];
+	    //    for (short num : array) {
+	    //        if (sum - num >= 0 && seen[sum - num]) {
+	    //            return true;
+	    //        } else {
+	    //            seen[num] = true;
+	    //        }
+	    //    }
+	    //    return false;
+		//}
+		
+			//return maximal positive number for which there is negative image
+			//if there not such numbers at all the method return -1
+			
+		
+		
+	//	public static short getMaxPositiveWithNegativeReflect(short[] array) {
+	//	    short maxPositive = -1;
+	//	    boolean[] seen = new boolean[Short.MAX_VALUE+1];
+
+	//	    for (short num : array) {
+	//	        if (num >= 0) {
+	//	            short negativeImage = (short) (-1 * num);
+	//	            if (negativeImage <= Short.MAX_VALUE && seen[(short)negativeImage]) { //
+	//	                maxPositive = num > maxPositive ? num : maxPositive;
+	//	            }
+	//	        } else {
+	//	            seen[(short)-1 * num] = true;
+	//	        }
+	//	    }
+
+	//	    return maxPositive;
+	//	}
+
+
+	public static short getMaxPositiveWithNegativeReflect(short[] array) {
+		short res=-1;
+		byte helper[]=new byte[Short.MIN_VALUE];
+		short candidade=-1;
+		for (int i=0; i<array.length; i++) {
+			candidade=(short) Math.abs(array[i]);
+			if (array[i]<0) {
+				if (helper[candidade]==1 && candidade>res) {
+					res=candidade;
+				} else if (helper[candidade]==0) {
+					helper[candidade]=-1;
+				}
+			}
+		}
+		
+		return res;
 		
 	}
 	
+	public static <T> int binarySearch(T [] array, T key, Comparator<T> comp) {
+		int leftIndex=0;
+		int rightIndex=array.length-1;
+		int middleIndex=(rightIndex-leftIndex)/2;
+		int compRes=0;
+		while (leftIndex<=rightIndex && (compRes=comp.compare(key, array[middleIndex]))!=0) {
+			if (compRes>0) {
+				//move to right
+				leftIndex=middleIndex+1;
+			} else {
+				rightIndex=middleIndex-1;
+			}
+			middleIndex=(leftIndex+rightIndex)/2;
+		}
+		return leftIndex>rightIndex ? -1 : middleIndex;
+		
+	}
 }
